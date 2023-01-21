ede3af41b75abdc13061889761d00aacdd975be7
SatishKumarRaizada <satish.kumar@maersk.com>
2023-01-21 23:02:29 +0530

    Example added

--- lib/dialogue.dart
+++ lib/dialogue.dart
@@ -1,7 +1,19 @@
 library dialogue;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// A Dialogue.
+class MyDialogue {
+  /// Returns beatiful alert dialogue.
+  static showDialogue(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return showGeneralDialog(
+      context: context,
+      pageBuilder: (context, _, __) {
+        return Container(
+          height: height * 0.6,
+        );
+      },
+    );
+  }
 }
